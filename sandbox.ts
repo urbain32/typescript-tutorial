5b08084be66e307ec509fa8af68f63a68a2b25d6
urbain32 <urbainq32@gmail.com>
2023-07-25 08:30:40 +0200

    using tsc sandbox.ts -w in terminal

--- sandbox.ts
+++ sandbox.ts
@@ -8,3 +8,4 @@ inputs.forEach((input) => {
 
 // for this typescript to be seen in any browser it needs to be compiled in js
 // using this code in terminal: tsc.cmd or tsc (name of the typescript file)
+// tsc.cmd or tsc (name of the typescript file) -w this will keep watching if there is any change and keep compling
