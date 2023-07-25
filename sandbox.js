ed44bf2808c6b5ab57c48388ddcbce0e3d293a91
urbain32 <urbainq32@gmail.com>
2023-07-25 08:23:38 +0200

    compliling our type script code

--- sandbox.js
+++ sandbox.js
@@ -1,7 +1,6 @@
-const character = "mario";
+var character = "mario";
 console.log(character);
-const inputs = document.querySelectorAll("input");
+var inputs = document.querySelectorAll("input");
 console.log(inputs);
-
 // for this typescript to be seen in any browser it needs to be compiled in js
 // using this code in terminal: tsc.cmd or tsc (name of the typescript file)
