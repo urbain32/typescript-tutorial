f867ef63afe9d4706bc11ffae164b8f4b4472ac7
urbain32 <urbainq32@gmail.com>
2023-07-26 15:22:27 +0200

    declaring a number variable in typescript

--- sandbox.ts
+++ sandbox.ts
@@ -1,3 +1,7 @@
 let character = "urban";
 // in typescrit the type we give a variable can not be change eg:
 // character=20 this show character is assignedas a string
+
+let age = 20;
+// same ting here if we make age another type it will not work
+// age = '20 ' asssigned to type number
