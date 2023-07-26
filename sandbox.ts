456a186bcbe0df2bfd9949206ceb79dd69fc118f
urbain32 <urbainq32@gmail.com>
2023-07-26 15:20:46 +0200

    declaring variable in typescript

--- sandbox.ts
+++ sandbox.ts
@@ -1,11 +1,3 @@
-const character = "urban";
-console.log(character);
-const inputs = document.querySelectorAll("input");
-console.log(inputs);
-inputs.forEach((input) => {
-  console.log(input);
-});
-
-// for this typescript to be seen in any browser it needs to be compiled in js
-// using this code in terminal: tsc.cmd or tsc (name of the typescript file)
-// tsc.cmd or tsc (name of the typescript file) -w this will keep watching if there is any change and keep compling
+let character = "urban";
+// in typescrit the type we give a variable can not be change eg:
+// character=20 this show character is assignedas a string
