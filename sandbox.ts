54225a430b57a5ffdd15839df900567927fd17fa
urbain32 <urbainq32@gmail.com>
2023-07-28 07:47:26 +0200

    dleclaring an array

--- sandbox.ts
+++ sandbox.ts
@@ -6,3 +6,8 @@ let isLogin: boolean;
 // we can't say
 // age = "10"; coz age is number
 // isLogin=23 coz isLogin is boolean
+
+// for arrays
+let ninjas: string[];
+// we can't say declare the array in any other type which is not a string
+// ninjas = ['urban',10] coz 10 is a number
