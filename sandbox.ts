038464f85f3aba9a42be1218f876867ddc2c352d
urbain32 <urbainq32@gmail.com>
2023-07-28 07:55:27 +0200

    pushing an array

--- sandbox.ts
+++ sandbox.ts
@@ -11,3 +11,7 @@ let isLogin: boolean;
 let ninjas: string[];
 // we can't say declare the array in any other type which is not a string
 // ninjas = ['urban',10] coz 10 is a number
+// if we want to push on the array we have to declare
+// ninjas.push("string");
+let numbers: number[] = [];
+numbers.push(2);
