86b4716197e8ae92c32c1f2ac51c4a2baa895c98
urbain32 <urbainq32@gmail.com>
2023-08-02 08:48:50 +0200

    trying something with array using reduce map

--- sandbox.js
+++ sandbox.js
@@ -18,3 +18,12 @@ console.log(mixed);
 var ninja;
 ninja = { age: "25", name: true };
 console.log(ninja);
+var ninjas = [
+    { id: 1, a: 0 },
+    { id: 2, a: 4 },
+    { id: 3, a: 5 },
+];
+var b = ninjas === null || ninjas === void 0 ? void 0 : ninjas.reduce(function (total, row) { return total + row.a; }, 0);
+var c = b * 0.18;
+var d = b - c;
+console.log("resultb", b, c, d);
