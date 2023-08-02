3794c3616180409294748032a0c14eaab12b4928
urbain32 <urbainq32@gmail.com>
2023-08-02 09:15:05 +0200

    usinginclude in our tsconfing.json at the end

--- app.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-console.log("first");
-// even ourtside any file we create outside our src will also be created in public to solve this
-// we use include in our tsconfing.json at the end
--- app.ts
+++ app.ts
@@ -1,3 +1,3 @@
-console.log("first");
+console.log("first0000");
 // even ourtside any file we create outside our src will also be created in public to solve this
 // we use include in our tsconfing.json at the end
