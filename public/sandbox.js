62ac629962734c91fc2aca976a39df341994ac37
urbain32 <urbainq32@gmail.com>
2023-08-02 09:19:01 +0200

    compiling made easy

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,2 +1,2 @@
 "use strict";
-console.log("ninja");
+console.log("ninja"); //  now to compile we will not need to write tsc.cmd (name  of the file) we just write tsc.cmd to compile
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,1 +1,1 @@
-console.log("ninja");
+console.log("ninja"); //  now to compile we will not need to write tsc.cmd (name  of the file) we just write tsc.cmd to compile
