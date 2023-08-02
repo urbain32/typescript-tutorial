5d09f0f9678eb6912aeaffea0701202eb6cd0c62
urbain32 <urbainq32@gmail.com>
2023-08-02 09:11:38 +0200

    creating ts file outside src and public

---
+++ app.js
@@ -0,0 +1,4 @@
+"use strict";
+console.log("first");
+// even ourtside any file we create outside our src will also be created in public to solve this
+// we use include in our tsconfing.json at the end
---
+++ app.ts
@@ -0,0 +1,3 @@
+console.log("first");
+// even ourtside any file we create outside our src will also be created in public to solve this
+// we use include in our tsconfing.json at the end
