af8da76db14169c3206738fa2baa5e964515f4cf
urbain32 <urbainq32@gmail.com>
2023-08-02 09:16:47 +0200

    compiling file outside src

--- app.ts
+++ app.ts
@@ -1,3 +1,4 @@
 console.log("first0000");
 // even ourtside any file we create outside our src will also be created in public to solve this
 // we use include in our tsconfing.json at the end
+// this will not be compiled automotaclly but other which are in src will be
