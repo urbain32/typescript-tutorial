7b568ff46adbc26be32d5cdf75114c0e6b2372e4
urbain32 <urbainq32@gmail.com>
2023-08-04 08:35:54 +0200

    creating two function

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,22 +1,8 @@
 "use strict";
-// function declaration
-let names;
-names = () => {
-    console.log("first name");
+// Type Aliases
+const logDetails = (uid, item) => {
+    console.log(`${item} has a uid of ${uid}`);
 };
-// if we say names = 'kkioko' this will be an error coz names has been defined as  function
-// best way to declare a function
-const sum = (a, b, c = 10) => {
-    //void helps when a function dont return anything
-    // adding a default value
-    // c is sting or number coz doesn't have a default value we use ?
-    console.log(a + b);
-    console.log(c);
+const greet = (user) => {
+    console.log(`${user.name} says hello`);
 };
-// sum(2, "2"); this will not work coz b is a string
-sum(2, 2, "20"); // now the value of c will be '20' not 10
-const minus = (a, b) => {
-    return a - b;
-};
-// result will become a function  which means result can not be a string or any other type than a function
-let result = minus(5, 7);
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,23 +1,9 @@
-// function declaration
-let names: Function;
-names = () => {
-  console.log("first name");
-};
-// if we say names = 'kkioko' this will be an error coz names has been defined as  function
+// Type Aliases
 
-// best way to declare a function
+const logDetails = (uid: string | number, item: string) => {
+  console.log(`${item} has a uid of ${uid}`);
+};
 
-const sum = (a: number, b: number, c: number | string = 10): void => {
-  //void helps when a function dont return anything
-  // adding a default value
-  // c is sting or number coz doesn't have a default value we use ?
-  console.log(a + b);
-  console.log(c);
+const greet = (user: { uid: string | number; name: string }) => {
+  console.log(`${user.name} says hello`);
 };
-// sum(2, "2"); this will not work coz b is a string
-sum(2, 2, "20"); // now the value of c will be '20' not 10
-const minus = (a: number, b: number) => {
-  return a - b;
-};
-// result will become a function  which means result can not be a string or any other type than a function
-let result = minus(5, 7);
