9821a4ba7a3a1661ba19f1f9dca817c61ed0e390
urbain32 <urbainq32@gmail.com>
2023-08-05 13:58:20 +0200

    creating a function signature that return void.

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,18 +1,2 @@
-// Type Aliases helps creating our own type so that we can use them in many place and reduce the codes
-//example
-type StringOrNum = string | number;
-type objectWithNameAndUid = { name: string; uid: StringOrNum };
-
-const logDetails = (uid: StringOrNum, item: string) => {
-  console.log(`${item} has a uid of ${uid}`);
-};
-
-const greet = (user: objectWithNameAndUid) => {
-  console.log(`${user.name} says hello`);
-};
-// now we can re-use our type
-
-const greetAgain = (user: objectWithNameAndUid, uid: StringOrNum) => {
-  console.log(`${user.name} with uid of ${uid} says hello`);
-};
-greetAgain({ name: "Urban", uid: 12 }, "12");
+// example 1
+let greet: (a: string, b: string) => void; // Function Signatures
