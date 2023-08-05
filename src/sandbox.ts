8456c22dace448de46548d26ac39a240348ad92e
urbain32 <urbainq32@gmail.com>
2023-08-05 14:01:06 +0200

    using our function signature to generate an error

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,6 +1,7 @@
 // example 1
 let greet: (a: string, b: string) => void; // Function Signatures
 greet = (name: string, age: string) => {
+  // if we assign any variable to something that is not a string will generate an error on our function
   console.log(`${name} is ${age} old`);
 };
 greet("urban", "20");
