6c6785c5a7bc90e28f0c3fa4541af002cede1953
urbain32 <urbainq32@gmail.com>
2023-08-05 13:59:11 +0200

    using our function signature

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,2 +1,6 @@
 // example 1
 let greet: (a: string, b: string) => void; // Function Signatures
+greet = (name: string, age: string) => {
+  console.log(`${name} is ${age} old`);
+};
+greet("urban", "20");
