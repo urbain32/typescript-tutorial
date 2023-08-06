ffa316d7bcbb832e20e1b4b4f8e7c2ae6e7b292f
urbain32 <urbainq32@gmail.com>
2023-08-06 12:11:53 +0200

    this will be an error coz anchor might be null

--- public/app.js
+++ public/app.js
@@ -1,1 +1,4 @@
 "use strict";
+//  how to slve typescript not displaying href
+const anchor = document.querySelector("a");
+console.log(anchor.href); // this will be an error coz anchor might be null
---
+++ src/app.ts
@@ -0,0 +1,4 @@
+//  how to slve typescript not displaying href
+
+const anchor = document.querySelector("a");
+console.log(anchor.href); // this will be an error coz anchor might be null
