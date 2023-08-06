56ee183b0e3c8012eb426aab7767960f0aee8229
urbain32 <urbainq32@gmail.com>
2023-08-06 12:16:16 +0200

    solving anchor tag being null

--- public/app.js
+++ public/app.js
@@ -1,4 +1,11 @@
 "use strict";
 //  how to slve typescript not displaying href
 const anchor = document.querySelector("a");
-console.log(anchor.href); // this will be an error coz anchor might be null
+// console.log(anchor.href); // this will be an error coz anchor might be null
+// solution 1
+if (anchor) {
+    console.log(anchor.href);
+}
+// solution 2
+console.log(anchor === null || anchor === void 0 ? void 0 : anchor.href);
+// solution 3 adding ! const anchor = document.querySelector("a")!;
--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,10 @@
 //  how to slve typescript not displaying href
-
 const anchor = document.querySelector("a");
-console.log(anchor.href); // this will be an error coz anchor might be null
+// console.log(anchor.href); // this will be an error coz anchor might be null
+// solution 1
+if (anchor) {
+  console.log(anchor.href);
+}
+// solution 2
+console.log(anchor?.href);
+// solution 3 adding ! const anchor = document.querySelector("a")!;
