9960e5d50e4b7f4b90e9ff6d50373837ed21b64e
urbain32 <urbainq32@gmail.com>
2023-08-07 10:53:33 +0200

    this ts will know it coz form is an html element

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,2 @@
-//  how to slve typescript not displaying href
-const anchor = document.querySelector("a");
-// console.log(anchor.href); // this will be an error coz anchor might be null
-// solution 1
-if (anchor) {
-  console.log(anchor.href);
-}
-// solution 2
-console.log(anchor?.href);
-// solution 3 adding ! const anchor = document.querySelector("a")!;
+// <form class="new-item-form">
+// const form = document.querySelector("form")!; this ts will know it coz form is an html element
