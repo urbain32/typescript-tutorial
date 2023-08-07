50dfb5b01187a164cdba23b1832da8bfdd484997
urbain32 <urbainq32@gmail.com>
2023-08-07 15:22:26 +0200

    we can even change the alue from invOne

--- public/app.js
+++ public/app.js
@@ -12,11 +12,15 @@ class Invoice {
 }
 const invOne = new Invoice("mario", "work on mario website", 250);
 const invTwo = new Invoice("luigi", "work on luigi website", 300);
+console.log(invOne, invTwo);
 // putting our invoice in an array
 let invoices = [];
 invoices.push(invOne);
 invoices.push(invTwo);
 console.log(invoices);
+invOne.client = "urban";
+invTwo.amount = 400;
+console.log(invOne, invTwo);
 const form = document.querySelector(".new-item-form");
 // adding the inputs using their id
 const type = document.querySelector("#type");
--- src/app.ts
+++ src/app.ts
@@ -15,12 +15,16 @@ class Invoice {
 }
 const invOne = new Invoice("mario", "work on mario website", 250);
 const invTwo = new Invoice("luigi", "work on luigi website", 300);
+console.log(invOne, invTwo);
 
 // putting our invoice in an array
 let invoices: Invoice[] = [];
 invoices.push(invOne);
 invoices.push(invTwo);
 console.log(invoices);
+invOne.client = "urban";
+invTwo.amount = 400;
+console.log(invOne, invTwo);
 
 const form = document.querySelector(".new-item-form") as HTMLFormElement;
 // adding the inputs using their id
