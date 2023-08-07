c7991e8e1069d5d3459b1c865af9d6459662af27
urbain32 <urbainq32@gmail.com>
2023-08-07 15:13:31 +0200

    adding values to our class

--- src/app.ts
+++ src/app.ts
@@ -13,6 +13,8 @@ class Invoice {
     return `${this.client} owes $${this.amount} for this ${this.details}`;
   }
 }
+const invOne = new Invoice("mario", "work on mario website", 250);
+const invTwo = new Invoice("luigi", "work on luigi website", 300);
 
 const form = document.querySelector(".new-item-form") as HTMLFormElement;
 // adding the inputs using their id
