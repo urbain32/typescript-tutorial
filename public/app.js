f62208eec276ac7c01bd541e7c7829838ebbd435
urbain32 <urbainq32@gmail.com>
2023-08-07 15:19:10 +0200

    putting our invoice in an array

--- public/app.js
+++ public/app.js
@@ -12,6 +12,11 @@ class Invoice {
 }
 const invOne = new Invoice("mario", "work on mario website", 250);
 const invTwo = new Invoice("luigi", "work on luigi website", 300);
+// putting our invoice in an array
+let invoices = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+console.log(invoices);
 const form = document.querySelector(".new-item-form");
 // adding the inputs using their id
 const type = document.querySelector("#type");
--- src/app.ts
+++ src/app.ts
@@ -16,6 +16,12 @@ class Invoice {
 const invOne = new Invoice("mario", "work on mario website", 250);
 const invTwo = new Invoice("luigi", "work on luigi website", 300);
 
+// putting our invoice in an array
+let invoices: Invoice[] = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+console.log(invoices);
+
 const form = document.querySelector(".new-item-form") as HTMLFormElement;
 // adding the inputs using their id
 const type = document.querySelector("#type") as HTMLSelectElement;
