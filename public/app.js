71a9b1c9241c13c8c05783fa6dec75d5f7ba978f
urbain32 <urbainq32@gmail.com>
2023-08-07 11:09:57 +0200

    this is how we add inputs field for our form

--- public/app.js
+++ public/app.js
@@ -3,4 +3,9 @@
 // const form = document.querySelector("form")!; this ts will know it coz form is an html element
 //when we use a class the typpe script  diplay it as an elemnt because it doesnt no the type  tos olve it we use as HTMLFormElement
 const form = document.querySelector(".new-item-form");
-console.log(form.children);
+// console.log(form.children);
+// adding the inputs using their id
+const type = document.querySelector("#type");
+const tofrom = document.querySelector("#tofrom");
+const details = document.querySelector("#details");
+const amount = document.querySelector("#amount");
--- src/app.ts
+++ src/app.ts
@@ -2,4 +2,9 @@
 // const form = document.querySelector("form")!; this ts will know it coz form is an html element
 //when we use a class the typpe script  diplay it as an elemnt because it doesnt no the type  tos olve it we use as HTMLFormElement
 const form = document.querySelector(".new-item-form") as HTMLFormElement;
-console.log(form.children);
+// console.log(form.children);
+// adding the inputs using their id
+const type = document.querySelector("#type") as HTMLSelectElement;
+const tofrom = document.querySelector("#tofrom") as HTMLInputElement;
+const details = document.querySelector("#details") as HTMLInputElement;
+const amount = document.querySelector("#amount") as HTMLInputElement;
