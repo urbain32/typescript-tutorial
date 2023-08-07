3931159efc4cf4511281bb2ea3ce99f9acdce9a4
urbain32 <urbainq32@gmail.com>
2023-08-07 10:57:11 +0200

    using class of a form

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,4 @@
 // <form class="new-item-form">
 // const form = document.querySelector("form")!; this ts will know it coz form is an html element
+//when we use a class the typpe script  diplay it as an elemnt because it doesnt no the type  tos olve it we use as HTMLFormElement
+const form = document.querySelector(".new-item-form") as HTMLFormElement;
