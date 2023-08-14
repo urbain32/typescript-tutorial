03b1014a96dd30a6d575789b9b56f43e184c1a61
urbain32 <urbainq32@gmail.com>
2023-08-14 16:56:31 +0200

    importing into app

--- src/app.ts
+++ src/app.ts
@@ -1,33 +1,6 @@
-// creating an interface
-interface IsPerson {
-  name: string;
-  age: number;
-  speak(a: string): void;
-  spend(a: number): number;
-}
-
-// creating a variable to use our interface
-const me: IsPerson = {
-  name: "Urban",
-  age: 26,
-  speak(text: string): void {
-    console.log(text);
-  },
-  spend(amount: number): number {
-    console.log("the amount is ", amount);
-    //coz it is a number we must return
-    return amount;
-  },
-};
-// let see our variable me
-console.log(me);
-// another thing is that it can be used everywhere
-const greetPerson = (person: IsPerson) => {
-  console.log("Name of the person is ", person.name);
-};
-greetPerson(me);
-
 import { Invoice } from "./classes/Invoice.js";
+import { Payment } from "./classes/Payment.js";
+import { HasFormatter } from "./interfaces/HasFormatter.js";
 
 const invOne = new Invoice("mario", " on the work done on the website", 250);
 const invTwo = new Invoice("luigi", "on the work done on the website", 300);
