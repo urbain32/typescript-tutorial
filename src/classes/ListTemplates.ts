eba14f25e87a02603d751f29c5ab266f4c08797a
urbain32 <urbainq32@gmail.com>
2023-08-22 09:54:01 +0200

    creating a list in our render

--- src/classes/ListTemplates.ts
+++ src/classes/ListTemplates.ts
@@ -2,5 +2,7 @@ import { HasFormatter } from "../interfaces/HasFormatter.js";
 
 export class ListTemplates {
   constructor(private container: HTMLDListElement) {}
-  render(item: HasFormatter, heading: string, pos: "start" | "end") {}
+  render(item: HasFormatter, heading: string, pos: "start" | "end") {
+    const list = document.createElement("li");
+  }
 }
