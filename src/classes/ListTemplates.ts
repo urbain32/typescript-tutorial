a1df9ef338d874f902c44f5efc7b52f1e80afabf
urbain32 <urbainq32@gmail.com>
2023-08-22 09:46:11 +0200

    adding what will be render

--- src/classes/ListTemplates.ts
+++ src/classes/ListTemplates.ts
@@ -1,3 +1,6 @@
+import { HasFormatter } from "../interfaces/HasFormatter.js";
+
 export class ListTemplates {
   constructor(private container: HTMLDListElement) {}
+  render(item: HasFormatter, heading: string, pos: "start" | "end") {}
 }
