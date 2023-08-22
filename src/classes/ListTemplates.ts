5bedb1738c06915c25f9f747c7ae6f3d8c2b6a1b
urbain32 <urbainq32@gmail.com>
2023-08-22 09:55:24 +0200

    creating an h4 that we will append to our list

--- src/classes/ListTemplates.ts
+++ src/classes/ListTemplates.ts
@@ -4,5 +4,6 @@ export class ListTemplates {
   constructor(private container: HTMLDListElement) {}
   render(item: HasFormatter, heading: string, pos: "start" | "end") {
     const list = document.createElement("li");
+    const h4 = document.createElement("h4");
   }
 }
