7ab9278970c7ccf25f12c47f06721af3a25cdec4
urbain32 <urbainq32@gmail.com>
2023-08-22 09:56:07 +0200

    now connnectinting our h4 to our heading

--- src/classes/ListTemplates.ts
+++ src/classes/ListTemplates.ts
@@ -5,5 +5,6 @@ export class ListTemplates {
   render(item: HasFormatter, heading: string, pos: "start" | "end") {
     const list = document.createElement("li");
     const h4 = document.createElement("h4");
+    h4.innerText = heading;
   }
 }
