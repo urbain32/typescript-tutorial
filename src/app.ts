3dbcad0a1f71eb92c17cb3574c3c6a4e3b9103da
urbain32 <urbainq32@gmail.com>
2023-08-28 15:19:40 +0200

    using enum will change the strirng in it to numb.

--- src/app.ts
+++ src/app.ts
@@ -39,7 +39,7 @@ console.log(docOne.name); // now the error goes away
 
 // using generics with interfaces
 // let say we want our data to be type  we want we have to use genercis
-// using Enum helps us not to keep remembering is like using a select
+// using Enum change string to number
 enum ResourceType {
   BOOK,
   AUTHOR,
